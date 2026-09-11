9e18245e7bc54c5d796bed56c2e2b0130f9a34cd
Peterafolabi-dev <afolabiprosper061@gmail.com>
2026-09-11 10:48:44 +0100

    hhhh

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.templatetags.static import static
 from django.utils import timezone
+from datetime import timedelta
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
